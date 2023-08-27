f96499d3feac28b48e14d3865d43f30e8a5f15bf
akiss9211 <akiss9211@gmail.com>
2023-08-27 19:49:29 -0400

    currency inventory added

--- portfolio-page/src/components/Projects.js
+++ portfolio-page/src/components/Projects.js
@@ -4,6 +4,7 @@ import './dndinventory.css';
 import { useState, useEffect } from 'react';
 import Card from './Card';
 import Inventory from './Inventory';
+import Currencystorage from './Currencystorage';
 
 function Projects() {
  //storing API JavaScript object data into a state managed array
@@ -12,25 +13,35 @@ function Projects() {
   const [inventory, setInventory] = useState([]);
  //state to hold and update current filter string from input
   const [filter, setfilter] = useState();
+  //state for currency data
+  const [currency, setCurrency] = useState({ platinum: 0, gold: 0, silver: 0, copper: 0 })
  //gets local storage inventory save and sets inventory state with data
- //by parsing into JS objects
- //checks to see if inventory save exists in local storage before retrieving
-  useEffect(() => {
-    const localInventory = localStorage.getItem('inventory-items');
-    if (localInventory !== null) { 
-      setInventory(JSON.parse(localInventory));
-    }
-  }, []);
-
- //sends inventory state data to local storage
-  useEffect(() => {
-    if(inventory.length) {
-      localStorage.setItem('inventory-items', JSON.stringify(inventory))
-    } else {
-      localStorage.clear();
-    }
-  }, [inventory]);
- 
+  //by parsing into JS objects
+  
+ //checks to see if inventory and currency save exists in local storage before retrieving
+ useEffect(() => { 
+  const localCurrency = localStorage.getItem('currency-items');
+  const localInventory = localStorage.getItem('inventory-items');
+  if (localCurrency !== null) { 
+    setCurrency(JSON.parse(localCurrency));
+  }
+  if (localInventory !== null) { 
+    setInventory(JSON.parse(localInventory));
+  }
+ }, []);
+ //sends inventory and currency state data to local storage
+ useEffect(() => {
+  if(currency.platinum > 0 || currency.gold > 0 || currency.silver > 0 || currency.copper > 0) {
+    localStorage.setItem('currency-items', JSON.stringify(currency))
+  } else {
+    localStorage.clear();
+  }
+  if(inventory.length) {
+    localStorage.setItem('inventory-items', JSON.stringify(inventory))
+  } else {
+    localStorage.clear();
+  }
+}, [inventory, currency]);
  //API fetch to return items as JS objects in promises
   useEffect(() => {
     fetch('https://www.dnd5eapi.co/api/equipment')
@@ -57,8 +68,11 @@ function Projects() {
                 <p>This app was created out of a fun project idea for my D&D group! It starts by fetching the equipment endpoint of the <b>https://www.dnd5eapi.co/</b> API. After the data is fetched, it is then saved to the items state by parsing the data into JSON objects. I store the value from the input box into a different piece of state, to be used as a filter, before mapping out which items need rendered into the search list. These items have a button that allow you to add them to the inventory, onClick, by mapping those objects along side a UUID and storing them in the inventory state. Everytime the inventory updates, the inventory state is saved into local storage so that your items persists through closing out the webpage and refreshing. The items have a button attatched to remove unwanted items by calling the filter method and filtering out the object with the specific UUID value passed to it by the button click. I still have plans to keep developing this app with other improvements as I have already made a list of features I would like to implement! If you care to see updates as they progress check my github repo!</p>
               </div>
               <div className='component-container-invapp'>
-                <Card items={items} setInventory={setInventory} inventory={inventory} filter={filter} setfilter={setfilter} />
-                <Inventory inventory={inventory} setInventory={setInventory} />
+                <Currencystorage currency={currency} setCurrency={setCurrency}/>
+                <div className='search-inv-components'>
+                  <Card items={items} setInventory={setInventory} inventory={inventory} filter={filter} setfilter={setfilter} />
+                  <Inventory inventory={inventory} setInventory={setInventory} />
+                </div>
               </div>
             </div>
         </div>
